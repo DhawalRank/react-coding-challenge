e5209e1473a14630c6d29b54c14af647f168a8e2
Dhawal Rank <sojitradhawal@gmail.com>
2018-05-05 09:44:07 -0400

    design: add loading spineer component

---
+++ image-mania/src/components/helpers/Loading/ScaleLoader.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { ScaleLoader } from "react-spinners";
+import "./Loading.css";
+
+export const CustomScaleLoader = () => {
+  return (
+    <div className="Loading__div--centered">
+      <ScaleLoader
+        color="#4A90E2"
+        height={150}
+        width={15}
+        radius={50}
+        margin="2px"
+        loading={true}
+      />
+    </div>
+  );
+};
